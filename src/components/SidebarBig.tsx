d7a0fd3a7f2fb23e79d52514dceb3226446c67ec
kens0serious <kens0serious@gmail.com>
2023-03-03 01:32:19 -0500

    Big sidebar

--- src/components/SidebarBig.tsx
+++ src/components/SidebarBig.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
 import styled from 'styled-components'
+import Logo from './Logo'
+import NavLinks from './NavLinks'
 
 const SidebarBig = () => {
-  return <Wrapper>SidebarBig</Wrapper>
+  return (
+    <Wrapper>
+      <div className="sidebar-container show-sidebar">
+        <div className="content">
+          <header>
+            <Logo />
+          </header>
+          <NavLinks></NavLinks>
+        </div>
+      </div>
+    </Wrapper>
+  )
 }
 
 const Wrapper = styled.aside`
@@ -10,6 +23,69 @@ const Wrapper = styled.aside`
 
   @media (min-width: 992px) {
     display: block;
+    box-shadow: 1px 0px 0px 0px rgba(0, 0, 0, 0.1);
+
+    .sidebar-container {
+      min-height: 100vh;
+      height: 100%;
+      width: 300px;
+      margin-left: -300px;
+      transition: var(--transition);
+    }
+
+    .content {
+      position: sticky;
+      top: 0;
+    }
+
+    .show-sidebar {
+      margin-left: 0;
+    }
+
+    header {
+      height: 6rem;
+      display: flex;
+      align-items: center;
+      padding-left: 2rem;
+    }
+    .nav-links {
+      padding-top: 3rem;
+      display: flex;
+      flex-direction: column;
+    }
+
+    .nav-link {
+      display: flex;
+      align-items: center;
+      color: var(--clr-main);
+      padding: 1rem 0;
+      padding-left: 4rem;
+      text-transform: capitalize;
+      transition: var(--transition);
+    }
+
+    .nav-link:hover {
+      background: var(--clr-background-secondary);
+      color: var(--clr-highlight-1);
+      padding-left: 4.5rem;
+    }
+    .nav-link:hover .icon {
+      color: var(--clr-highlight-1);
+    }
+    .icon {
+      font-size: 1.5rem;
+      margin-right: 1rem;
+      display: grid;
+      place-items: center;
+      transition: var(--transition);
+    }
+
+    .active {
+      color: var(--clr-highlight-1);
+    }
+    .active .icon {
+      color: var(--clr-highlight-1);
+    }
   }
 `
 
