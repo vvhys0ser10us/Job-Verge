978cd87d3ad2786c915cf27a8f9105a7a8ed01f8
kens0serious <kens0serious@gmail.com>
2023-03-07 00:13:15 -0500

    Add pre-typed createAsyncThunk

--- src/utils/hooks.tsx
+++ src/utils/hooks.tsx
@@ -1,6 +1,11 @@
-import { Root } from 'react-dom/client'
+import { createAsyncThunk } from '@reduxjs/toolkit'
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
 import type { RootState, AppDispatch } from '../store'
 
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
 export const useAppDispatch: () => AppDispatch = useDispatch
+export const createAppAsyncThunk = createAsyncThunk.withTypes<{
+  state: RootState
+  dispatch: AppDispatch
+  rejectValue: string
+}>()
