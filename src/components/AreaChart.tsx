98039d58e09d3330c296f98235e2d4d1d89812e0
kens0serious <kens0serious@gmail.com>
2023-03-10 01:25:07 -0500

    AreaChart

--- src/components/AreaChart.tsx
+++ src/components/AreaChart.tsx
@@ -1,5 +1,38 @@
-const AreaChart = () => {
-  return <div>AreaChart</div>
+import {
+  ResponsiveContainer,
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+} from 'recharts'
+import { ChartData } from '../utils/types'
+
+type PropsType = {
+  data: ChartData[]
 }
 
-export default AreaChart
+const AreaChartComponent = ({ data }: PropsType) => {
+  return (
+    <ResponsiveContainer width="100%" height={300}>
+      <AreaChart data={data} margin={{ top: 50 }}>
+        <CartesianGrid strokeDasharray="3 3" stroke="#94a1b2" />
+        <XAxis
+          dataKey="date"
+          tick={{ fill: '#fffffe' }}
+          tickLine={{ stroke: '#94a1b2' }}
+        />
+        <YAxis
+          allowDecimals={false}
+          tick={{ fill: '#fffffe' }}
+          tickLine={{ stroke: '#94a1b2' }}
+        />
+        <Tooltip />
+        <Area type="monotone" dataKey="count" stroke="#593fa8" fill="#7f5af0" />
+      </AreaChart>
+    </ResponsiveContainer>
+  )
+}
+
+export default AreaChartComponent
--- src/components/ChartContainer.tsx
+++ src/components/ChartContainer.tsx
@@ -31,7 +31,7 @@ const ChartContainer = () => {
       <span onClick={() => setBarType(!barType)}>
         {barType ? 'Bar Chart' : 'Area Chart'}
       </span>
-      {barType ? <BarChart /> : <AreaChart />}
+      {barType ? <BarChart /> : <AreaChart data={data} />}
     </Wrapper>
   )
 }
